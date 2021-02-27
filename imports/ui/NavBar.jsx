13414424ae768ddb763b45f3b43e5b3aed9bc208
SiMBaIrk <segyrn@yandex.ru>
2021-02-27 21:21:22 +0800

    поправил форму логина, сделал возможность регистрации

--- imports/ui/NavBar.jsx
+++ imports/ui/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { useTracker } from "meteor/react-meteor-data";
 
 class NavBarBrand extends React.Component {
@@ -39,30 +39,77 @@ class NavBarBrand extends React.Component {
   }
   
   class LoginForm extends React.Component{
+    constructor(props){
+      super(props)
+
+      this.state = {isLogin: true, username: "", password: "", secondPassword: "", email: ""};
+      this.toggleRegister = this.toggleRegister.bind(this);
+    }
+
+    toggleRegister(){
+      this.setState(state => ({isLogin: !state.isLogin}));
+    }
+
     render(){
-      return <form className="dropdown-menu p-4" style={{padding: "15px", paddingBottom: "10px"}}>
-            <div className="form-group">
-              <label htmlFor="exampleDropdownFormEmail2">Email address</label>
-              <input type="email" className="form-control" id="exampleDropdownFormEmail2" placeholder="email@example.com" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="exampleDropdownFormPassword2">Password</label>
-              <input type="password" className="form-control" id="exampleDropdownFormPassword2" placeholder="Password" />
-            </div>
-            <div className="form-check">
-              <input type="checkbox" className="form-check-input" id="dropdownCheck2" />
-              <label className="form-check-label" htmlFor="dropdownCheck2">
-                Remember me
-              </label>
-            </div>
-            <button type="submit" className="btn btn-primary">Sign in</button>
-          </form>
+      let loginButton;
+      let checkBoxRemember;
+      let userName;
+      let secondPassword;
+      if (this.state.isLogin){
+        loginButton = <button type="submit" className="btn btn-primary">Log in</button>;
+
+        checkBoxRemember = <div className="form-check">
+        <input type="checkbox" className="form-check-input" id="dropdownCheck2" />
+        <label className="form-check-label" htmlFor="dropdownCheck2">
+          Remember me
+        </label>
+        </div>;
+        userName = "";
+        secondPassword = "";
+      }else {
+        loginButton = <button type="submit" className="btn btn-primary">Sign in</button>;
+        checkBoxRemember = "";
+
+        userName = <div className="form-group">
+          <label htmlFor="FormLogin">Login</label>
+          <input type="login" className="form-control" id="FormLogin" placeholder="username" />
+        </div>;
+
+        secondPassword = <div className="form-group">
+            <label htmlFor="FormConfirmPassword">Confirm-password</label>
+            <input type="password" className="form-control" id="FormConfirmPassword" placeholder="confirm password" />
+          </div>;
+      }
+
+      return <Fragment> 
+        <form className="dropdown-menu" style={{padding: "15px", paddingBottom: "10px", minWidth: "240px"}}>
+        <ul className="nav nav-pills">
+          <li role="presentation" className={this.state.isLogin?("active"):("")}><a onClick={this.toggleRegister}>Login</a>
+          </li>
+          <li role="presentation" className={this.state.isLogin?(""):("active")}><a onClick={this.toggleRegister}>Register</a>
+          </li>
+        </ul>
+          {userName}
+          <div className="form-group">
+            <label htmlFor="FormEmail">Email address</label>
+            <input type="email" className="form-control" id="FormEmail" placeholder="email@example.com" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="FormPassword">Password</label>
+            <input type="password" className="form-control" id="FormPassword" placeholder="Password" />
+          </div>
+          {secondPassword}
+          {checkBoxRemember}
+          {loginButton}
+        </form>
+        </Fragment>
     }
   }
   
   class LoginButton extends React.Component{
     render(){
-      return <li className="dropdown"><a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Login <span className="caret"></span></a>
+      return <li className="dropdown">
+          <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Login <span className="caret"></span></a>
           <LoginForm />
         </li>
     }
@@ -71,6 +118,7 @@ class NavBarBrand extends React.Component {
   class NavBarNavRight extends React.Component{
     render(){
       return <ul className="nav navbar-nav navbar-right">
+        <li><p class="navbar-text">Already have an account?</p></li>
         <LoginButton />
       </ul>
     }
